7c33527cd6c59d8b3d567033e2f0c02829973b2d
Abhijeet Rastogi <arastogi@linkedin.com>
2018-10-06 06:39:06 -0700

    Better tests for printer.go

--- printer_test.go
+++ printer_test.go
@@ -4,7 +4,9 @@ import (
 	"testing"
 )
 
-func TestGetSortedCounters(t *testing.T) {
+func populateCounterData() {
+	clearCounters()
+
 	ensureCounter("key", "field_value")
 	countersMap.counters["key"]["field_value"].Incr(1)
 
@@ -17,11 +19,18 @@ func TestGetSortedCounters(t *testing.T) {
 	countersMap.counters["key"]["field_value3"].Incr(1)
 	countersMap.counters["key"]["field_value3"].Incr(1)
 
+	// For testing sorting
+	ensureCounter("1_key", "field_value3")
+	ensureCounter("2_key", "field_value3")
+}
+
+func TestGetSortedCounters(t *testing.T) {
+	populateCounterData()
+
 	sorted := getSortedCounters(countersMap.counters["key"])
 
 	// field_value3 has the highest count, it should be first
 	if sorted[0].Key != "field_value3" {
 		t.Errorf("Expected field_value3 to be the first element, got %s", sorted[0].Key)
 	}
-
 }
