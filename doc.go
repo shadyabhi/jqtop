53ade3a58b009e2deed6db533e5072543ae29977
Abhijeet Rastogi <arastogi@linkedin.com>
2018-10-06 03:20:50 -0700

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+
+jqtop is a tool to analyze json logs or equivalent stream of inputs in realtime.
+
+Read: https://github.com/shadyabhi/jqtop/blob/master/README.md for more info
+*/
+package main
