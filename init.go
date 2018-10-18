07d50fe155e4c2b1c4faa0f57ca23e0e07816d74
Abhijeet Rastogi <me@abhi.host>
2018-10-18 23:57:31 +0530

    Set Args.ParallelProc to 4 by default

--- init.go
+++ init.go
@@ -10,7 +10,7 @@ func init() {
 	// Defaults
 	Args.Interval = 1
 	Args.MaxResult = 10
-	Args.ParallelProc = 1
+	Args.ParallelProc = 4
 
 	parseErrors = ratecounter.NewRateCounter(time.Duration(Args.Interval) * time.Second)
 }
